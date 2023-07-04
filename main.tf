60b26544406b480a7cbfbd50fbb424cad5521c8e
Rafael Galante Gutiérrez <ragalgut@outlook.com>
2023-07-04 16:50:01 +0200

    Create test lb

--- main.tf
+++ main.tf
@@ -37,6 +37,19 @@ terraform {
 variable "resource_group_name" {}
 variable "location" {}
 
+# Variables lb
+
+variable "type" {}
+variable "frontend_subnet_id" {}
+variable "frontend_private_ip_address_allocation" {}
+variable "frontend_private_ip_address" {}
+variable "lb_sku" {}
+variable "name_lb" {}
+variable "remote_port" {}
+variable "lb_port" {}
+variable "lb_probe" {}
+
+
 # Variables tags
 variable "tags" {}
 
@@ -48,4 +61,27 @@ module "resource-group" {
   resource_group_name = var.resource_group_name
   location = var.location
   tags = var.tags
+}
+
+
+module "mylb" {
+  source                                 = "git@github.com:ragalgut/az-tf-module-load-balancer.git"
+
+  type                                   = var.type
+  frontend_subnet_id                     = var.frontend_subnet_id
+  frontend_private_ip_address_allocation = var.frontend_private_ip_address_allocation
+  frontend_private_ip_address            = var.frontend_private_ip_address
+  lb_sku                                 = var.lb_sku
+  location                               = var.location
+  name                                   = var.name_lb
+
+  remote_port = var.remote_port
+
+  lb_port = var.lb_port
+
+  lb_probe = var.lb_probe
+
+  tags = var.tags
+
+  #depends_on = [azurerm_resource_group.test]
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,6 +1,31 @@
 # resource group
 resource_group_name = "rg-test00"
 location = "West Europe"
+
+# lb
+
+type = "private"
+frontend_subnet_id = "/subscriptions/4c595a39-ef54-4e5c-af06-8ffd1963ac5e/resourceGroups/labs/providers/Microsoft.Network/virtualNetworks/vnet-test/subnets/default"
+frontend_private_ip_address_allocation = "static"
+frontend_private_ip_address = "10.0.0.7"
+lb_sku = "Basic"
+name_lb = "testlb"
+
+remote_port = {
+  ssh = ["Tcp", "22"]
+}
+
+lb_port = {
+  http  = ["80", "Tcp", "80"]
+   https = ["443", "Tcp", "443"]
+}
+
+lb_probe = {
+  http  = ["Tcp", "80", ""]
+  http2 = ["Http", "1443", "/"]
+}
+
+
 tags = {
   "test5" = "test6"
 }
